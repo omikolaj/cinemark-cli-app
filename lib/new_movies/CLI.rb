3298a54b370acc5a3d6ba2c6228b16ef7354aa6a
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-11-03 09:23:30 +0000

    defined call, list_new_movies and menu methods

--- lib/new_movies/CLI.rb
+++ lib/new_movies/CLI.rb
@@ -1,7 +1,31 @@
 class NewMovies::CLI
 
   def call
-    puts "hello world"
+    list_new_movies
+    menu
   end
+
+  def list_new_movies
+    puts "Movies Coming Soon:"
+
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the movie you would like to see more details about or type movies to see a list of movies again or type exit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on movie 1.."
+      when "2"
+        puts "more info on movie 2..."
+      when "movies"
+        list_upcoming_movies
+      else
+        puts "Not sure what you want, type list or exit"
+    end
+  end
+end
 
 end
