b1d2d9504d6167febfb509ee6293d5c673fb56f4
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-11-03 10:06:51 +0000

    Created a shell for a working movie class

--- lib/new_movies/CLI.rb
+++ lib/new_movies/CLI.rb
@@ -10,13 +10,16 @@ class NewMovies::CLI
   end
 
   def menu
+
     list_new_movies
     input = nil
+    NewMovies::Movie.all1
     while input != "exit"
       puts "Enter the number of the movie you would like to see more details about or type movies to see a list of movies again or type exit:"
       input = gets.strip.downcase
 
       if input.to_i > 0
+
         puts NewMovies::Movie.all[input.to_i - 1]
       elsif input == "list"
         list_new_movies
--- lib/new_movies/movie.rb
+++ lib/new_movies/movie.rb
@@ -1,10 +1,38 @@
 class NewMovies::Movie
+attr_accessor :title, :runtime, :genre, :url
+  @@all = []
 
   def self.all
+    @@all
+  end
+
+  def self.all1
     puts "
       1. Star Wars
-      2. Watchdogs
+      2. Wonder
       "
+      movie_1 = self.new
+      @@all << movie_1
+      movie_1.title = "Star Wars"
+      movie_1.runtime = "150 min"
+      movie_1.genre = "Action, Adventure, Sci-Fi, Fantasy"
+      movie_1.url = "https://www.cinemark.com/star-wars-the-last-jedi"
+
+      #movie_2 = self.new
+      #movie_2.title = "Wonder"
+      #movie_2.runtime = "113 min"
+      #movie_2.genre = "Drama"
+      #movie_2.url = "https://www.cinemark.com/wonder"
+
+  end
+
+  def self.movie_details
+    movie_1 = self.new
+    @@all << movie_1
+    movie_1.title = "Star Wars"
+    movie_1.runtime = "150 min"
+    movie_1.genre = "Action, Adventure, Sci-Fi, Fantasy"
+    movie_1.url = "https://www.cinemark.com/star-wars-the-last-jedi"
   end
 
 
