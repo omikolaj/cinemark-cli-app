26e29d18f66bec806c7dbefdfd2cf7c96cff1fd2
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-11-05 17:07:18 +0000

    appearance

--- lib/new_movies/CLI.rb
+++ lib/new_movies/CLI.rb
@@ -13,7 +13,7 @@ WIDTH = 70
 
 
   def wrap(s)
-       s.gsub(/(.{1,#{WIDTH}})(\s+|\Z)/, "\\1\n")
+       s.gsub(/(.{1,#{WIDTH}})(\s+|\Z)/, "\\1\n  ")
      end
 
   def call
@@ -39,37 +39,37 @@ WIDTH = 70
   def menu
     list_new_movies
     input = nil
+    puts "What movie do you want more details about?"
+    print "Please select a number: "
+    input = gets.strip.downcase
     if input == "exit"
       goodbye
-      puts "Enter the number of the movie you would like to see more details about or type movies to see a list of movies again or type exit:"
-      input = gets.strip.downcase
-
-      if input.to_i > 0
+    elsif input.to_i > 0
         movie_details(input)
-      elsif input == "list"
+    elsif input == "list"
         list_new_movies
-      else
+    else
         puts "not sure what you meant"
       end
     end
-  end
+
 
   def movie_details(input)
     details = NewMovies::Movie.find_movie_by_index(input)
-    puts wrap("#{movie.title}.upcase")
+    puts center("#{details.title}.upcase")
     puts " "
-    puts "Title: #{details.title}" if details.title
-    puts "URL: #{details.url}" if details.url
-    puts "Runetime: #{details.runtime}" if details.runtime
-    puts "Genre: #{details.genre}" if details.genre
-    puts "Release Date: #{details.release_date}" if details.release_date
-    puts "Cast: #{details.cast}" if details.cast
-    puts "Director: #{details.director}" if details.director
-    puts "Movie Site: #{details.movie_site}" if details.movie_site
-    puts "Synopsis: #{details.synopsis}" if details.synopsis
-    puts "Rating: #{details.rating}" if details.rating
+    puts "  Title: #{details.title}" if details.title
+    puts "  URL: #{details.url}" if details.url
+    puts "  Runetime: #{details.runtime}" if details.runtime
+    puts "  Rating: #{details.rating}" if details.rating
+    puts "  Genre: #{details.genre}" if details.genre
+    puts "  Release Date: #{details.release_date}" if details.release_date
+    puts wrap("  Cast: #{details.cast}") if details.cast
+    puts "  Director: #{details.director}" if details.director
+    puts "  Movie Site: #{details.movie_site}" if details.movie_site
+    puts wrap("\n  Synopsis: #{details.synopsis}") if details.synopsis
     puts " "
-    puts wrap("END OF DETAILS")
+    puts center("END OF DETAILS")
     puts " "
   end
 
